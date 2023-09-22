294de805078891171129841dc4b0442afe369213
alisaghir1 <alisaghir543@gmail.com>
2023-09-22 10:58:08 +0300

    step 5

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var express = require('express');
 
 var app = express();
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
+
+
+console.log(movies);
 app.get('/test', function(req, res){
    res.send(`status:${res.statusCode}, message:"ok"`);
 });
@@ -26,6 +36,19 @@ app.get('/search', function(req, res){
         res.status(500).json(` status: ${res.statusCode}, error: true, message: 'you have to provide a search`);
       }
 });
+app.get('/movies/add', function(req, res){
+    res.send(`status:${res.statusCode}, message: anything`);
+ });
+ app.get('/movies/get', function(req, res){
+    res.send(`status:${res.statusCode}, message: ${movies.map(e=>{return ` title: ${e.title}, year: ${e.year},rating: ${e.rating} ||`})}`);
+ });
+ app.get('/movies/edit', function(req, res){
+    res.send(`status:${res.statusCode}, message: anything`);
+ });
+ app.get('/movies/delete', function(req, res){
+    res.send(`status:${res.statusCode}, message: anything`);
+ });
+
 
 
 app.listen(8080);
