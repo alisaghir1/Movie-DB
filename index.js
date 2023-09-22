d2a87c09c54e6c8f357e76bf8f8304fd4f4bcae9
alisaghir1 <alisaghir543@gmail.com>
2023-09-22 10:05:26 +0300

    step 3

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res){
-   res.send("ok");
+app.get('/test', function(req, res){
+   res.send(`{status:${res.statusCode}, message:"ok"}`);
 });
 
+app.get('/time', function(req, res){
+    let date=new Date();
+    let time=`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
+    res.send(`{status:${res.statusCode}, message: ${time}}`);
+ });
+
 app.listen(8080);
+
