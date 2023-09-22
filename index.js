ec02b0c3caad8e3674f1a51a81bf84bc57e16467
alisaghir1 <alisaghir543@gmail.com>
2023-09-22 13:57:50 +0300

    i just edited everything in an easier way to understand more

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 
+
 const movies = [
     { title: 'Jaws', year: 1975, rating: 8, id: 1},
     { title: 'Avatar', year: 2009, rating: 7.8 , id: 2},
@@ -19,44 +20,73 @@ app.get('/test', function(req, res){
 app.get('/time', function(req, res){
     let date=new Date();
     let time=`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`
-    res.send(`status:${res.statusCode}, message: ${time}`);
+    res.status(200).send(`status:${res.statusCode}, message: ${time}`);
  });
+
  app.get('/hello/:id', function(req, res){
      let id= req.params.id;
-    res.send(`status:${res.statusCode}, message: hello ${id}`);
+    res.status(200).send(`status:${res.statusCode}, message: hello ${id}`);
  });
+
  app.get('/hello', function(req, res){
    res.send(`status:${res.statusCode}, message: hello`);
 });
+
 app.get('/search', function(req, res){
     let search=  req.query.s;
     if (search) {
-        res.status(200).json( `status: ${res.statusCode}, message: 'ok', data: ${search} `);
+        res.status(200).send( `status: ${res.statusCode}, message: 'ok', data: ${search} `);
       } else {
-        res.status(500).json(` status: ${res.statusCode}, error: true, message: 'you have to provide a search`);
+        res.status(500).send(` status: ${res.statusCode}, error: true, message: 'you have to provide a search`);
       }
 });
+
 app.get('/movies/add', function(req, res){
-    res.send(`status:${res.statusCode}, message: anything`);
+    res.status(200).send(`status:${res.statusCode}, message: anything`);
  });
+
  app.get('/movies/get', function(req, res){
-    res.send(`status:${res.statusCode}, message: ${movies.map(e=>{return ` title: ${e.title}, year: ${e.year},rating: ${e.rating} ||`})}`);
+    const response = {
+      status: res.statusCode,
+      data: movies
+    }
+    res.send(response);
  });
  app.get('/movies/edit', function(req, res){
-    res.send(`status:${res.statusCode}, message: anything`);
+    res.status(200).send(`status:${res.statusCode}, message: anything`);
  });
  app.get('/movies/delete', function(req, res){
-    res.send(`status:${res.statusCode}, message: anything`);
+   res.status(200).send(`status:${res.statusCode}, message: anything`);
  });
+
  app.get('/movies/get/by-date', function(req, res){
-   res.send(`status:${res.statusCode}, message: ${movies.sort((a, b)=>{return a.year - b.year}).map(e=>{return ` title: ${e.title}, year: ${e.year},rating: ${e.rating} ||`})}`);
-});
-app.get('/movies/get/by-rating', function(req, res){
-   res.send(`status:${res.statusCode}, message: ${movies.sort((a, b)=>{return b.rating - a.rating}).map(e=>{return ` title: ${e.title}, year: ${e.year},rating: ${e.rating} ||`})}`);
-});
-app.get('/movies/get/by-title', function(req, res){
-   res.send(`status:${res.statusCode}, message: ${movies.sort((a, b)=>{  if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;if (a.title.toLowerCase() > b.title.toLowerCase()) return 1; return 0;}).map(e=>{return ` title: ${e.title}, year: ${e.year},rating: ${e.rating} ||`})}`);
-});
+   movies.sort((a, b)=>{return a.year - b.year})
+   const response = {
+      statues: res.statusCode,
+      data : movies
+   }
+   res.status(200).send(response);
+ });
+
+ app.get('/movies/get/by-rating', function(req, res){
+   movies.sort((a, b)=>{return b.rating - a.rating})
+   const response = {
+      statues: res.statusCode,
+      data : movies
+   }
+   res.status(200).send(response);
+ });
+
+ app.get('/movies/get/by-tittle', function(req, res){
+   movies.sort((a, b)=>{  if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;if (a.title.toLowerCase() > b.title.toLowerCase()) return 1; return 0;})
+   const response = {
+      statues: res.statusCode,
+      data : movies
+   }
+   res.status(200).send(response);
+ });
+ 
+
 app.get('/movies/get/id/:id', function(req, res){
    let id= parseInt(req.params.id);
    let movie= movies.find((movie)=> movie.id === id);
@@ -71,4 +101,3 @@ app.get('/movies/get/id/:id', function(req, res){
 
 
 app.listen(8080);
-
