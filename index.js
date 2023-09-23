2251f0f54ebe992297834908f5e897d31a85734d
alisaghir1 <alisaghir543@gmail.com>
2023-09-23 18:55:42 +0300

    forget the slash

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ const movies = [
 ];
 
 console.log(movies);
-app.get('api/test', function (req, res) {
+app.get('/api/test', function (req, res) {
   res.send(`status:${res.statusCode}, message:"ok"`);
 });
 
-app.get('api/time', function (req, res) {
+app.get('/api/time', function (req, res) {
   let date = new Date();
   let time = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
   res.status(200).send(`status:${res.statusCode}, message: ${time}`);
 });
 
-app.get('api/hello/:id', function (req, res) {
+app.get('/api/hello/:id', function (req, res) {
   let id = req.params.id;
   res.status(200).send(`status:${res.statusCode}, message: hello ${id}`);
 });
 
-app.get('api/hello', function (req, res) {
+app.get('/api/hello', function (req, res) {
   res.send(`status:${res.statusCode}, message: hello`);
 });
 
-app.get('api/search', function (req, res) {
+app.get('/api/search', function (req, res) {
   let search = req.query.s;
   if (search) {
     res
@@ -44,7 +44,7 @@ app.get('api/search', function (req, res) {
   }
 });
 
-app.post('api/addmovies', function (req, res) {
+app.post('/api/addmovies', function (req, res) {
   let addTitle = req.query.title;
   let addYear = req.query.year;
   let addRating = req.query.rating || 4;
@@ -73,14 +73,15 @@ app.post('api/addmovies', function (req, res) {
   }
 });
 
-app.get('api/movies', function (req, res) {
+app.get('/api/movies', function (req, res) {
   const response = {
     status: res.statusCode,
     data: movies,
   };
   res.send(response);
 });
-app.put('api/editmovies/:id', function (req, res) {
+
+app.put('/api/editmovies/:id', function (req, res) {
   let editMoviesTitle = req.query.title;
   let editMoviesYear = req.query.year;
   let editMoviesRating = req.query.rating;
@@ -110,7 +111,7 @@ app.put('api/editmovies/:id', function (req, res) {
   });
 });
 
-app.delete('api/deletemovies/:id', function (req, res) {
+app.delete('/api/deletemovies/:id', function (req, res) {
   const movieId = parseInt(req.params.id);
   const movieIndex = movies.findIndex((movie) => movie.id === movieId);
   if (movieIndex === -1) {
@@ -130,7 +131,7 @@ app.delete('api/deletemovies/:id', function (req, res) {
   }
 });
 
-app.get('api/movies/by-date', function (req, res) {
+app.get('/api/movies/by-date', function (req, res) {
   movies.sort((a, b) => {
     return a.year - b.year;
   });
@@ -141,7 +142,7 @@ app.get('api/movies/by-date', function (req, res) {
   res.status(200).send(response);
 });
 
-app.get('api/movies/by-rating', function (req, res) {
+app.get('/api/movies/by-rating', function (req, res) {
   movies.sort((a, b) => {
     return b.rating - a.rating;
   });
@@ -152,7 +153,7 @@ app.get('api/movies/by-rating', function (req, res) {
   res.status(200).send(response);
 });
 
-app.get('api/movies/by-tittle', function (req, res) {
+app.get('/api/movies/by-tittle', function (req, res) {
   movies.sort((a, b) => {
     if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
     if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
@@ -165,7 +166,7 @@ app.get('api/movies/by-tittle', function (req, res) {
   res.status(200).send(response);
 });
 
-app.get('api/movies/id/:id', function (req, res) {
+app.get('/api/movies/id/:id', function (req, res) {
   let id = parseInt(req.params.id);
   let movie = movies.find((movie) => movie.id === id);
   if (!movie) {
