5ac808cfa4c3421d99a190957d51ac81643c4f82
alisaghir1 <alisaghir543@gmail.com>
2023-09-26 17:22:55 +0300

    step 13

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const movies = [
   { title: 'Brazil', year: 1985, rating: 8, id: 3 },
   { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2, id: 4 },
 ];
+const users = [
+  { username: 'John', password: '1234' },
+  { username: 'Jane', password: '5678' },
+];
+
+function authenticateUser(req, res, next) {
+  const { username, password } = req.headers;
+
+  const authenticatedUser = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (authenticatedUser) {
+    next();
+  } else {
+    res.status(401).json({ error: 'Authentication failed' });
+  }
+}
+
+app.use('/api/movies', authenticateUser);
 
 const movieSchema = new mongoose.Schema({
   title: String,
@@ -56,19 +76,19 @@ app.get('/api/search', function (req, res) {
 
 app.post('/api/addmovies', async (req, res) => {
   const { title, year, rating } = req.body;
-  // if (!title || !year) {
-  //   res
-  //     .status(403)
-  //     .send(
-  //       ` status: ${res.statusCode}, error: true, message: 'you have to provide a title and a year'`
-  //     );
-  // } else if (year.length < 4 || year.length > 4 || isNaN(year)) {
-  //   res
-  //     .status(403)
-  //     .send(
-  //       ` status: ${res.statusCode}, error: true, message: 'make sure you put 4 digits'`
-  //     );
-  // }
+  if (!title || !year) {
+    res
+      .status(403)
+      .send(
+        ` status: ${res.statusCode}, error: true, message: 'you have to provide a title and a year'`
+      );
+  } else if (year.length < 4 || year.length > 4 || isNaN(year)) {
+    res
+      .status(403)
+      .send(
+        ` status: ${res.statusCode}, error: true, message: 'make sure you put 4 digits'`
+      );
+  }
   try {
     const newMovie = await Movie.create({ title, year, rating });
     res.status(200).json({ message: 'Movie has been added successfully' });
@@ -191,6 +211,55 @@ app.get('/api/movies/id/:id', function (req, res) {
   } else {
     res.send(movie);
   }
+});
+
+app.post('/api/users', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: 'Username and password are required' });
+  }
+
+  const existingUser = users.find((user) => user.username === username);
+  if (existingUser) {
+    return res.status(409).json({ error: 'Username already exists' });
+  }
+
+  const newUser = { username, password };
+  users.push(newUser);
+
+  res.status(201).json({ message: 'User created successfully', user: newUser });
+});
+
+app.put('/api/users/:username', (req, res) => {
+  const { username } = req.params;
+  const { password } = req.body;
+
+  const userToUpdate = users.find((user) => user.username === username);
+
+  if (!userToUpdate) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  userToUpdate.password = password;
+
+  res.json({ message: 'User updated successfully', user: userToUpdate });
+});
+
+app.delete('/api/users/:username', (req, res) => {
+  const { username } = req.params;
+
+  const userIndex = users.findIndex((user) => user.username === username);
+
+  if (userIndex === -1) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  const deletedUser = users.splice(userIndex, 1)[0];
+
+  res.json({ message: 'User deleted successfully', user: deletedUser });
 });
 
 mongoose
